9180d3d1d3ac6f355a4800fa18f485b0a372ff16
Praful-87 <103850217+Praful-87@users.noreply.github.com>
2023-09-20 21:28:57 +0530

    landing page created

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,28 @@ function App() {
   console.log(data);
   return (
     <>
-      <h1>Home</h1>
+      <div
+        className="container"
+        style={{
+          height: "100vh",
+          border: "1px solid red",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          gap: "16px",
+        }}
+      >
+        <label htmlFor="input-field">
+          what programming language they’re looking for a job in.
+        </label>
+        <input
+          id="input-field"
+          type="text"
+          
+        />
+        <button className="serach-btn">Search</button>
+      </div>
     </>
   );
 }
